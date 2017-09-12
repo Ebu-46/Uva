eeff179c671a5d43665b66becc2d0ec76343542c
Ebu-46 <30809719+Ebu-46@users.noreply.github.com>
2017-09-12 08:47:22 +0600

    FirstAssinment
    fkpee

---
+++ Part1.java
@@ -0,0 +1,35 @@
+public class Part1{
+    
+    String findSimpleGene(String DNA){
+        
+        int start = DNA.indexOf("ATG");
+        int end = DNA.indexOf("TAA", start + 3);
+        if(end == -1 || start == -1 || (end - start)%3 != 0)
+            return "";
+        else 
+            return DNA.substring(start, end + 3);
+    }
+    
+    void testSimpleGene(){
+        
+        String DNA = "ATACGATATTATACCCGAGAT";
+        String gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        DNA = "ATCGGATATCATAACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        DNA = "ATGACGATATTATACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        DNA = "ATGACGATATAATATAACTGCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        DNA = "ATGACGATAGTAATATAACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+    }
+}
